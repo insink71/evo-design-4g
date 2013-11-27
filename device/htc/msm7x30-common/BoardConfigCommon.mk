4d7d0969294da89be49bb4e45a5150bddd383583
Robert Sink <insink71@gmail.com>
2013-11-27 15:17:15 -0500

    Add folder back

---
+++ device/htc/msm7x30-common/BoardConfigCommon.mk
@@ -0,0 +1,105 @@
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Common stuff
+TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
+DEVICE_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay
+
+# Platform
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Vendor
+BOARD_VENDOR := htc
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+
+USE_MALLOC_ALIGNMENT := 16
+TARGET_HAVE_TSLIB := false
+
+TARGET_EXTRA_CFLAGS += $(call cc-option,-march=armv7-a -mtune=cortex-a8)
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+
+# Qcom
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+TARGET_QCOM_AUDIO_VARIANT := legacy
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# Adreno
+BOARD_USES_ADRENO_200 := true
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_SF_BYPASS := true
+TARGET_HAVE_BYPASS := true
+TARGET_USES_OVERLAY := true
+TARGET_GRALLOC_USES_ASHMEM := true
+TARGET_USES_GENLOCK := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
+TARGET_NO_HW_VSYNC := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DNO_UPDATE_PREVIEW
+BOARD_EGL_CFG := device/htc/msm7x30-common/egl.cfg
+
+# Camera
+BOARD_USES_HTC_CAMERA := true  # Added by Dm47021
+CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+ifeq ($(BOARD_USES_HTC_GB_CAMERA), true)
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+else
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB
+endif
+
+# Audio
+BOARD_PREBUILT_LIBAUDIO := false
+BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
+BOARD_USES_QCOM_AUDIO_RESETALL := true
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+# BT
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/msm7x30-common/bluetooth/vnd_msm7x30.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/htc/msm7x30-common/bluetooth/include
+
+# GPS
+TARGET_QCOM_GPS_VARIANT := legacy
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
+
+# Ril
+BOARD_USE_NEW_LIBRIL_HTC := true
+BOARD_USES_LEGACY_RIL := true
+
+# Webkit
+TARGET_FORCE_CPU_UPLOAD := true
